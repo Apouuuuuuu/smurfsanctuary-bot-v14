04067058ac18b8f8a16310f228224bd3a4fa48cb
Theo <gagelintheo2@gmail.com>
2023-04-07 12:01:01 +0200

    Git ignore config

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: 'MTA3OTg1MTk3OTEyOTQ5OTc3OQ.GVsu6X.lSZzN6yhgZX07qnfKc_mY0kptgGJY8mkHpDcI8',
+    openaikey: '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',
+    twitchClientId: "6asxguro1cgvdcdipvdk2fd1do63jg",
+    twitchClientSecret: "gt9dt13pvveory2mhg3din7ndrkrst",
+    twitchAccessToken: "wv37h8623m7jaip8wm0rivg6gznhnx",
+    discordChannelId: "1085490627027947610",
+    twitchUsername: "apou__",
+  };
+
